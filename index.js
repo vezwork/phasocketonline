1e3e5a87692520467b0996e270676560feaaa4d0
vezwork <key.draw@gmail.com>
2015-05-31 00:38:52 -0600

    prepped for heroku. (I hope) 3

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(5000, function(){
+    console.log('listening on *:5000');
 });
